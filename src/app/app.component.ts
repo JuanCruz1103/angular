5902ef9dec49016a2df22e4298962597c85be655
JuanCruz1103 <juanantoniocr1103@gmail.com>
2025-02-21 08:39:47 -0600

    aaaaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { TarjetaProductoComponent } from "./tarjeta-producto/tarjeta-producto.component";
-import {InterpolacionComponent} from "./interpolacion/interpolacion.component"
+// import {InterpolacionComponent} from "./interpolacion/interpolacion.component"
+import { ComponentePadreComponent } from './Componentes/componente-padre/componente-padre.component';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet, TarjetaProductoComponent],
- imports: [RouterOutlet, InterpolacionComponent],
+imports: [RouterOutlet, ComponentePadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
